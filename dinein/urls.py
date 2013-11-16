3e3015d548763a3f3f5826dd79dd515e309ea717
Milton <madandat@gmail.com>
2013-11-16 10:32:13 +0200

    init index template

--- dinein/urls.py
+++ dinein/urls.py
@@ -5,8 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'dinein.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'dinein.web.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
