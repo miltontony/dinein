58257403ec8153abc1455de058b352d03bd18595
Milton <madandat@gmail.com>
2013-11-16 12:11:42 +0200

    change db file

--- dinein/settings.py
+++ dinein/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'dinein.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dinein.db'),
     }
 }
 
