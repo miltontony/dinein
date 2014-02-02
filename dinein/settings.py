e770e2bb72b998a62bc1a93600d8bea0af5ea4c0
Milton <madandat@gmail.com>
2014-02-02 10:54:44 +0200

    added apps to settings

--- dinein/settings.py
+++ dinein/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = (
 
     'dinein',
     'social_auth',
+    'south',
+    'gunicorn',
+    'djcelery',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ production_settings.py
@@ -0,0 +1,11 @@
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dinein',                      # Or path to database file if using sqlite3.
+        'USER': 'dinein',                      # Not used with sqlite3.
+        'PASSWORD': 'dinein',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
